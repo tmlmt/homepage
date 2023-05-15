b6b3a0798276645cb255baeb0898bee71649c374
Thomas Lamant <tom@tmlmt.com>
2023-05-15 16:07:04 +0200

    feat: add page transition

--- app.vue
+++ app.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
     <NuxtLayout>
-      <NuxtPage />
+      <NuxtPage :transition="{ name: 'my', mode: 'out-in' }" />
     </NuxtLayout>
   </div>
 </template>
+
+<style>
+.my-enter-active,
+.my-leave-active {
+  transition: opacity 0.3s;
+}
+.my-enter,
+.my-leave-active {
+  opacity: 0;
+}
+</style>
