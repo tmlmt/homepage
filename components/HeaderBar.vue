8aa8d279c330e0831f72fdca10e8e424e7df7858
Thomas Lamant <tom@tmlmt.com>
2023-05-16 00:11:30 +0200

    fix(HeaderBar): link accessibility (#7)

--- components/HeaderBar.vue
+++ components/HeaderBar.vue
@@ -9,10 +9,10 @@ import logoSrc from '~/assets/logo.svg'
     </div>
     <div class="col-4 flex justify-content-end align-items-center">
       <NuxtLink to="/links" class="mx-2">Links</NuxtLink>
-      <a href="https://www.linkedin.com/in/thomas-lamant-9a28423b/" target="_blank"><i class="pi pi-linkedin mx-2" /></a>
-      <a href="https://instagram.com/tmlmt" target="_blank"><i class="pi pi-instagram mx-2" /></a>
-      <a href="https://github.com/tmlmt" target="_blank"><i class="pi pi-github mx-2" /></a>
-      <a href="mailto:tom@tmlmt.com" target="_blank"><i class="pi pi-envelope mx-2" /></a>
+      <a href="https://www.linkedin.com/in/thomas-lamant-9a28423b/" aria-label="LinkedIn profile" target="_blank"><i class="pi pi-linkedin mx-2" /></a>
+      <a href="https://instagram.com/tmlmt" aria-label="Instagram profile" target="_blank"><i class="pi pi-instagram mx-2" /></a>
+      <a href="https://github.com/tmlmt" aria-label="GitHub profile" target="_blank"><i class="pi pi-github mx-2" /></a>
+      <a href="mailto:tom@tmlmt.com" aria-label="Send me an email" target="_blank"><i class="pi pi-envelope mx-2" /></a>
     </div>
   </div>
 </template>
