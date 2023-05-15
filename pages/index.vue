8893da0de5827caf6188289977fbe1a0038f2572
Thomas Lamant <tom@tmlmt.com>
2023-05-15 16:04:55 +0200

    fix(index): improve text

--- pages/index.vue
+++ pages/index.vue
@@ -14,25 +14,29 @@ defineOgImageStatic({ description: "Thomas Lamant's homepage" })
       <a href="https://www.orsted.com" class="emphasis" target="_blank">Ørsted</a>.
     </p>
     <p>
-      When onshore, I love exploring all sorts of ideas. And it happens
-      sometimes that I feel like writing an article about it in my
+      When onshore, I love exploring all sorts of ideas. And sometimes I feel like
+      writing an article about them in my
       <a href="https://blog.tmlmt.com" class="emphasis" target="_blank">blog</a>.
     </p>
     <p>
-      Manual creatity has taken many forms over the years (woodworking, yoga, etc.),
-      but I've particularly discoved a passion for leatherwork, that I hope one day to
-      to bring to a broader public through my own brand.
+      Manual creatity has taken many forms for me over the years (woodworking, yoga, etc.).
+      I've particularly discoved a passion for leatherwork, that I hope one day to
+      to bring to a broader public through my own brand,
+      <a href="https://www.maraa.dk" class="emphasis" target="_blank">Marå</a>.
     </p>
     <p>
       Since writing my first website more than 20 years ago in 2002, I've been
       fascinated by the power of software programming. Things have become much more
-      powerful now, but also much more complex. While I play around in <span class="emphasis">Python/Pandas</span> for
-      small tasks or data analysis, it is web development that I've been exploring a lot recently,
-      especially with the <a href="https://nuxt.com" class="emphasis" target="_blank">Nuxt</a> framework (Vue, Vite, Node.JS, Typescript).
+      powerful nowadays, but also much more complex. When I don't play around with
+      <span class="emphasis">Python/Pandas</span> for small tasks or data analysis, it is
+      web development that I'm exploring, especially the
+      <span class="emphasis">Nuxt</span> framework
+      (Vue, Vite, Node.JS, Typescript). It powered this homepage, but also my latest
+      project, <a href="https://www.bodegamap.com" class="emphasis" target="_blank">Bodega Map</a>.
     </p>
     <p>
       See the menu in the upper right corner for relevant links, and shortcuts to my social media accounts.
-      The best way to contact me is by sending an e-mail. Cheers !
+      The best way to contact me is by sending an e-mail. ✌
     </p>
   </div>
 </template>
