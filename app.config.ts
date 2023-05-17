d35cfe47ec966a42123826f4faf75159fb790ec8
Thomas Lamant <tom@tmlmt.com>
2023-05-17 23:33:06 +0200

    feat: add Umami analytics

---
+++ app.config.ts
@@ -0,0 +1,8 @@
+export default defineAppConfig({
+  umami: {
+    host: 'https://stats.lamanufactu.re',
+    id: 'b44384cc-b5b2-4422-b8bb-012b18e852ee',
+    ignoreLocalhost: true,
+    version: 2
+  }
+})
