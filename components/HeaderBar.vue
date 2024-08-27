525927e609d4ebec6062633c609298eed94b430a
Thomas Lamant <tom@tmlmt.com>
2024-08-27 10:55:07 +0200

    feat(HeaderBar): point contact link to contact page on blog

--- components/HeaderBar.vue
+++ components/HeaderBar.vue
@@ -12,7 +12,7 @@ import logoSrc from '~/assets/logo.svg'
       <a href="https://www.linkedin.com/in/thomas-lamant-9a28423b/" aria-label="LinkedIn profile" target="_blank"><i class="pi pi-linkedin mx-2" /></a>
       <a href="https://instagram.com/tmlmt" aria-label="Instagram profile" target="_blank"><i class="pi pi-instagram mx-2" /></a>
       <a href="https://github.com/tmlmt" aria-label="GitHub profile" target="_blank"><i class="pi pi-github mx-2" /></a>
-      <a href="mailto:tom@tmlmt.com" aria-label="Send me an email" target="_blank"><i class="pi pi-envelope mx-2" /></a>
+      <a href="https://blog.tmlmt.com/contact" aria-label="Send me a message" target="_blank"><i class="pi pi-envelope mx-2" /></a>
     </div>
   </div>
 </template>
