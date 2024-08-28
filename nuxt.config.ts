ed3c5e1092cdee5960620ac2039249a466b61b9a
Thomas Lamant <tom@tmlmt.com>
2024-08-28 23:33:19 +0200

    feat: replace unnused pwa features by generated icons
    Generated using https://realfavicongenerator.net/

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,24 @@
 export default defineNuxtConfig({
   extends: ['nuxt-umami'],
 
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+        { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#024FCA' }
+      ],
+      meta: [
+        { name: 'apple-mobile-web-app-title', content: 'tmlmt' },
+        { name: 'application-name', content: 'tmlmt' },
+        { name: 'msapplication-TileColor', content: '#024FCA' },
+        { name: 'theme-color', content: '#024FCA' }
+      ]
+    }
+  },
+
   build: {
     transpile: ['primevue']
   },
@@ -16,41 +34,11 @@ export default defineNuxtConfig({
 
   devtools: { enabled: true },
 
-  modules: ['@nuxt/image-edge', '@nuxtjs/robots', '@nuxtjs/sitemap', 'nuxt-og-image', '@kevinmarrec/nuxt-pwa'],
-
-  ogImage: {
-
-  },
+  modules: ['@nuxt/image-edge', '@nuxtjs/robots', '@nuxtjs/sitemap', 'nuxt-og-image'],
 
   postcss: {
     plugins: {
       'postcss-nesting': {}
-    }
-  },
-
-  pwa: {
-    icon: {
-      source: './public/logoSquare.png',
-      maskablePadding: 0,
-      maskableSource: './public/logoMaskable.png'
-    },
-    meta: {
-      mobileAppIOS: true,
-      appleStatusBarStyle: 'black-translucent',
-      name: 'Thomas Lamant',
-      theme_color: '#666666',
-      description: false,
-      ogTitle: false,
-      ogDescription: false,
-      ogImage: false,
-      ogUrl: false
-    },
-    manifest: {
-      short_name: 'tmlmt',
-      name: 'Thomas Lamant',
-      description:
-        "Thomas Lamant's homepage",
-      theme_color: '#666666'
     }
   },
 
