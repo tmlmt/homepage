c662a4213f0a0e4818a49c2787481f370f5da932
Thomas Lamant <tom@tmlmt.com>
2025-08-01 00:19:32 +0200

    chore: bump nuxt to v4 and all other dependencies to latest on Aug 1, 2025

--- app.config.ts
+++
@@ -1,9 +0,0 @@
-export default defineAppConfig({
-  umami: {
-    host: 'https://stats.lamanufactu.re',
-    id: '91e5f788-0a06-48d9-aaae-c7a06c2e13e2',
-    ignoreLocalhost: true,
-    version: 2,
-    useDirective: true
-  }
-})
