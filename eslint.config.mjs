caca68de83286f8e2c54b92d946339e47c7cb19d
Thomas Lamant <tom@tmlmt.com>
2025-08-05 16:17:29 +0200

    ci: fix eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,9 @@
 // @ts-check
-import withNuxt from ".nuxt/eslint.config.mjs";
+import withNuxt from "./.nuxt/eslint.config.mjs";
 
 export default withNuxt({
   rules: {
     "vue/singleline-html-element-content-newline": "off",
+    "vue/html-self-closing": "off",
   },
 });
